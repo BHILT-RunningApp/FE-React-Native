e80d1f1fee8283623bc2901d41f3123270f5f7ce
iddy00711 <idrisdodwell00711@gmail.com>
2019-12-11 11:32:15 -0500

    updated login page

---
+++ src/LoginPage.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import {StyleSheet, Text, View, TextInput, KeyboardAvoidingView, TouchableOpacity, AsyncStorage, Button} from 'react-native';
+import getUser from './Api.js';
+
+
+
+
+
+
+
+
+export default class Login extends React.Component{
+
+
+  
+       state = {
+       userName: '',
+       passWord: ''
+       }
+   
+
+
+        //sends user/password to api function and then goes to homepage
+    
+
+    login = () => {
+        
+        
+        const {userName, passWord} = this.state;
+        
+        getUser(userName, passWord).then((res)=>{
+            
+            if(res){
+
+                return this.props.navigation.navigate('HomePage');
+             }
+            
+        }
+          
+
+        )    
+
+    }
+
+   
+
+
+    render() {
+        const {userName, passWord} = this.state
+        return (
+            <KeyboardAvoidingView behavior='padding' style = {styles.wrapper}>
+
+                <View style = {styles.container}>
+
+                    <Text styles= {styles.header}/>
+
+                        <TextInput style = {styles.TextInput} placeholder= 'username'
+                        onChangeText = {(userName) => this.setState({userName})}/>
+
+
+
+                        <TextInput
+                        style = {styles.TextInput} placeholder= 'password'
+                        onChangeText = {(passWord) => this.setState({passWord})}
+
+                        />
+
+                    <Button
+                                style = {styles.btn}
+                                title="Log in"
+                                onPress={() => this.props.navigation.navigate('Home')}>
+                     </Button>
+
+
+                    <Button 
+                                style = {styles.btn}
+                                title="Sign Up"
+                                onPress={() => this.signUp(userName, email, passWord, currentLocation)}>
+                     </Button>
+     
+                </View>
+
+            </KeyboardAvoidingView>
+        )
+    }
+
+    
+
+}
+const styles = StyleSheet.create({
+
+    wrapper:{
+        flex:1,
+    },
+    container:{
+        flex:1,
+        alignItems :'center',
+        justifyContent: 'center',
+        paddingLeft:40,
+        paddingRight: 40,
+
+
+
+
+        
+    },
+    header:{
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginBottom: 60,
+
+
+
+    },
+    TextInput:{
+        marginBottom: 20,
+        padding: 20,
+        alignSelf: 'stretch'
+    },
+
+    Button:{
+        alignSelf:'stretch'
+    }
+
+
+
+
+
+})
+
+
+
+
+
